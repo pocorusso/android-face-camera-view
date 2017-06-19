60c9ff2925efb3d424330c69324869a01714fc22
pocorusso <pocorusso@gmail.com>
2017-06-19 14:33:56 -0500

    init

---
+++ app/src/main/java/com/pocorusso/bearbeard/FaceCamera.java
@@ -0,0 +1,13 @@
+package com.pocorusso.bearbeard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaceCamera extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_camera);
+    }
+}
